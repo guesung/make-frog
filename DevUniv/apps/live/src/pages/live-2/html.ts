f0db31b35f7e931f73e86e487b4b461368f9c6a0
guesung <gueit214@naver.com>
2024-10-25 22:18:18 +0900

    feat(수요코딩회): concat을 구현하여 지연 배열 합성

--- DevUniv/apps/live/src/pages/live-2/html.ts
+++ DevUniv/apps/live/src/pages/live-2/html.ts
@@ -1,26 +1,41 @@
 import { flat, map, pipe, reduce, zip } from '@fxts/core';
+import { escapeHtml } from '../live-2 copy/helper';
 
 function tag(strings: any, ...values: unknown[]): void {
   console.log(strings, values);
 }
 
-function html(strings: TemplateStringsArray, ...values: unknown[]) {
+function* concat(...args: any[]) {
+  for (const arg of args) {
+    yield* arg;
+  }
+}
+
+const html = (strings: TemplateStringsArray, ...values: unknown[]) =>
   // zip : 둘씩 하나의 튜플로 만들어주는 함수형 함수
-  return pipe(
+  pipe(
     zip(
       strings,
-      map((v) => `${v}`, values),
+      concat(
+        map((v) => escapeHtml(v), values),
+        [''],
+      ),
     ),
     flat,
     reduce((a, b) => a + b),
   );
-}
 
 export function main() {
   const a: 'a' = 'a';
   const b: 'b' = 'b';
   const c: 'c' = 'c';
 
-  const result: string = html`1${a}2${b}3${c}`;
+  const result: string = html`
+    <ul>
+      <li>${a}</li>
+      <li>${b}</li>
+      <li>${c}</li>
+    </ul>
+  `;
   console.log(result);
 }

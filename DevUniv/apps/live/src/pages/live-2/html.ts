d3a86f7b4e55fe04f88a5b0dd7e9a521113302ca
guesung <gueit214@naver.com>
2024-10-25 12:35:20 +0900

    feat(수요코딩회): reduce로 하나의 문자열로 합쳐준다

--- DevUniv/apps/live/src/pages/live-2/html.ts
+++ DevUniv/apps/live/src/pages/live-2/html.ts
@@ -1,14 +1,18 @@
-import { flat, pipe, zip } from '@fxts/core';
+import { flat, pipe, reduce, zip } from '@fxts/core';
 
-function tag(strings: any, ...values: any[]): void {
+function tag(strings: any, ...values: unknown[]): void {
   console.log(strings, values);
 }
 
-function html(strings: TemplateStringsArray, ...values: unknown[]) {
+function html(strings: TemplateStringsArray, ...values: string[]) {
   values.push('');
 
   // zip : 둘씩 하나의 튜플로 만들어주는 함수형 함수
-  return pipe(zip(strings, values), flat);
+  return pipe(
+    zip(strings, values),
+    flat,
+    reduce((a, b) => a + b),
+  );
 }
 
 export function main() {
@@ -17,5 +21,5 @@ export function main() {
   const c: 'c' = 'c';
 
   const result: string = html`1${a}2${b}3${c}`;
-  console.log([...result]);
+  console.log(result);
 }

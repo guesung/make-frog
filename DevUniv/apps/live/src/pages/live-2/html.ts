9d3959b68fa2dda805d65046b4a53ecaec343128
guesung <gueit214@naver.com>
2024-10-25 12:32:41 +0900

    feat(수요코딩회): pipe + flat이용하여 2차원 배열을 하나로 펴기

--- DevUniv/apps/live/src/pages/live-2/html.ts
+++ DevUniv/apps/live/src/pages/live-2/html.ts
@@ -1,4 +1,4 @@
-import { zip } from '@fxts/core';
+import { flat, pipe, zip } from '@fxts/core';
 
 function tag(strings: any, ...values: any[]): void {
   console.log(strings, values);
@@ -7,7 +7,8 @@ function tag(strings: any, ...values: any[]): void {
 function html(strings: TemplateStringsArray, ...values: unknown[]) {
   values.push('');
 
-  return zip(strings, values);
+  // zip : 둘씩 하나의 튜플로 만들어주는 함수형 함수
+  return pipe(zip(strings, values), flat);
 }
 
 export function main() {
@@ -16,13 +17,5 @@ export function main() {
   const c: 'c' = 'c';
 
   const result: string = html`1${a}2${b}3${c}`;
-
-  console.log(result.next().value);
-  console.log(result.next().value);
-  console.log(result.next().value);
-  console.log(result.next().value);
-  console.log(result.next().value);
-  console.log(result.next().value);
+  console.log([...result]);
 }
-
-// zip : 둘씩 하나의 튜플로 만들어주는 함수형 함수

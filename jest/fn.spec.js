e025013968d41b7a49cfe1ebbd316bc992c38d96
guesung <gueit214@naver.com>
2024-11-04 14:06:49 +0900

    feat(jest): `beforeEach`

--- jest/fn.spec.js
+++ jest/fn.spec.js
@@ -1,20 +1,13 @@
 const fn = require("./fn.js");
 
+let value = 0;
+
+beforeEach(() => {
+  value = 10;
+});
+
 describe("add", () => {
-  test("두 숫자의 합을 더하여 반환한다.", () => {
-    expect(fn.add(1, 2)).toBe(3);
-  });
-
-  test("Mike의 이름을 3초 후에 반환한다.", (done) => {
-    const callback = (name) => {
-      expect(name).toBe("Mike");
-      done();
-    };
-    fn.getName(callback);
-  });
-
-  test("Mike의 나이를 3초 후에 반환한다.", async () => {
-    const age = await fn.getAge();
-    expect(age).toBe(25);
+  test("beforEach를 테스트한다.", () => {
+    expect(fn.add(value, 5)).toBe(15);
   });
 });
